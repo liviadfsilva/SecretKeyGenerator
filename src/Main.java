d640e81d405aeccd58557301115d2aaaa353d9f4
liviadfsilva <liviadfsilva@gmail.com>
2026-02-25 14:56:05 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.security.SecureRandom;
+import java.util.Base64;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        SecureRandom random = new SecureRandom();
+        byte bytes[] = new byte[32];
+        random.nextBytes(bytes);
+
+        String token = Base64.getUrlEncoder()
+                             .withoutPadding()
+                             .encodeToString(bytes);
+        
+        System.out.println(token);
+    }
+}
